79a7e3410e334ad8c87f3eec413a3cc64a40e3bc
Affracs <alexfrancois1501@gmail.com>
2025-08-01 05:55:02 +0000

    fix

--- print_number.c
+++ print_number.c
@@ -7,35 +7,39 @@
  *
  */
 
-int print_number_helper(int num)
+int print_number_helper(unsigned int n)
 {
-	int count = 0;
-	char digit;
+    int count = 0;
+    char digit;
 
-	if (num / 10)
-		count += print_number_helper(num / 10);
+    if (n / 10)
+        count += print_number_helper(n / 10);
 
-	digit = '0' + (num % 10);
-	count += write(1, &digit, 1);
+    digit = '0' + (n % 10);
+    count += write(1, &digit, 1);
 
-	return (count);
+    return count;
 }
 
 int print_number(va_list args)
 {
-	int num = va_arg(args, int);
-	int count = 0;
-	char neg = '-';
+    int num = va_arg(args, int);
+    int count = 0;
+    char neg = '-';
+    unsigned int n;
 
-	if (num < 0)
-	{
-		write(1, &neg, 1);
-		num = -num;
-		count++;
-	}
+    if (num < 0)
+    {
+        write(1, &neg, 1);
+        n = (unsigned int)(-1 * (num + 1)) + 1;
+        count++;
+    }
+    else
+    {
+        n = num;
+    }
 
-	count += print_number_helper(num);
-
-	return (count);
+    count += print_number_helper(n);
+    return count;
 }
 

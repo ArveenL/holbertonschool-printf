a7f571e3ead69cb429e433d23c9000682a44119e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2025-12-08 05:07:50 +0400

    0

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
  * _printf - cust.printf that looks for either a normal string
@@ -28,7 +29,7 @@ int _printf(const char *format, ...)
 
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			if (format[i] == 'c')
